67600d062392c51ce08256182f4ae1e65ed7f07c
Robert Down <robertdown@live.com>
2017-05-12 16:21:52 -0400

    Fix bootstrap require

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,4 @@
  * @license https://github.com/robertdown/html-helper/blob/master/LICENSE MIT
  */
 
-require_once dirname(__FILE__) . "/../vendor/autoload.php";
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
